ea56a27e783119e9298039293c41badd2bb1c2dd
Nicholas <nicholastengshin@hotmail.com>
2022-01-22 01:33:56 +0800

    Done with Exercise 1.2, Going to save it in same branch as Exercise 1.1. Too  bad I can't rename it.

--- src/App.js
+++ src/App.js
@@ -10,13 +10,34 @@ const Header = (props) => {
   )
   }
 
+  const Part = (props) => {
+    
+    return(
+      <div>
+        <p>{props.part} {props.exercises}</p>
+      </div>
+    )
+  }
+
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
+      <Part part = "Fundamentals of React" exercises = {10} />
+
+      <Part part = "Using props to pass data" exercises = {7}/>
+
+      <Part part = "State of a component" exercises = {14}/>
+
+    </div>
+    /*
+    <div>
+
+       <p>{props.part1} {props.exercises1}</p>
         <p>{props.part2} {props.exercises2}</p>
         <p>{props.part3} {props.exercises3}</p>
+  
     </div>
+    */
   )
 }
 
@@ -30,19 +51,18 @@ const Total = (props) =>{
 
 const App = (props) => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
+  //const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
+  //const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
+  //const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header course = {course} />
       
-      <Content part1 = {part1} exercises1 = {exercises1} part2 = {part2} exercises2 = {exercises2}
-       part3 = {part3} exercises3 = {exercises3} />
+      <Content />
       
       <Total TotalExercise = {exercises1 + exercises2 + exercises3} />
     </div>

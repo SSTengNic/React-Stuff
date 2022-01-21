0a63b2d5620b90d4feef8c125674e5d61df6cb15
Nicholas <nicholastengshin@hotmail.com>
2022-01-22 01:01:48 +0800

    Done with exercise 1.1, going to push to repo as a feautre branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import React from 'react'
 
-const Hello = (props) => { //This is called a "component"
+const Header = (props) => {
+  return(
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+  }
+
+const Content = (props) => {
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old </p>
+      <p>{props.part1} {props.exercises1}</p>
+        <p>{props.part2} {props.exercises2}</p>
+        <p>{props.part3} {props.exercises3}</p>
     </div>
   )
 }
 
-const App = ()=>{
-  const age = 10
-  const name = 'Soulen'
-
-  return (
-  <div>
-    <h1>Greetings!</h1>
-    <Hello name = {name} age = {age}/>
-  </div>
+const Total = (props) =>{
+  return(
+    <div>
+      <p>Number of Exercises {props.TotalExercise}</p>
+    </div>
   )
 }
+
+const App = (props) => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course} />
+      
+      <Content part1 = {part1} exercises1 = {exercises1} part2 = {part2} exercises2 = {exercises2}
+       part3 = {part3} exercises3 = {exercises3} />
+      
+      <Total TotalExercise = {exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
 
 export default App
